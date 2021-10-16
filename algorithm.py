56199bf0472beaab18b057cdeaaf39c26a252f55
Connor <connorbrook@googlemail.com>
2021-10-16 20:36:07 +0100

    refactored code

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,6 @@
 import parse_data
 import pandas as pd
 
-calendars = parse_data.get_files()
-people = parse_data.parse_files(calendars)
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
 
 def build_dataframe(elements: list, drop=True) -> pd.DataFrame:
     frames = []
@@ -20,8 +16,10 @@ def build_dataframe(elements: list, drop=True) -> pd.DataFrame:
 
 def group_names(df: pd.DataFrame) -> pd.DataFrame:
     lectures = []
-    for Day in days:
-        i = df.loc[df.Day == Day]
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+    for day in days:
+        i = df.loc[df.Day == day]
         lectures.append(
             i.groupby(["Day", "Start", "End", "Unit"], as_index=False).agg(
                 {"Names": " ".join}
