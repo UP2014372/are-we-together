3280a144b86b8201e8ca363bda7adfba2554e2d1
Connor <connorbrook@googlemail.com>
2021-10-16 19:36:18 +0100

    added drop conditional argument to build_dataframe, return hinting

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,5 @@
 import parse_data
 import pandas as pd
-from parse_data import get_files, parse_files
-from algorithm import build_dataframe, group_names
 import pandas as pd
 import pyperclip
 
@@ -10,20 +8,22 @@ people = parse_data.parse_files(calendars)
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 
-def build_dataframe(elements: list):
+def build_dataframe(elements: list, drop=True) -> pd.DataFrame:
     frames = []
     for element in elements:
         frames.append(pd.DataFrame(element).dropna())
 
     df = pd.concat(frames)
     df.reset_index(inplace=True, drop=True)
+    if drop:
+        df = df.loc[df.Type != "OL-Drop-In"]
     return df
 
 
-def group_names(df: pd.DataFrame):
+def group_names(df: pd.DataFrame) -> pd.DataFrame:
     lectures = []
     for Day in days:
-        i = df.loc[df["Day"] == Day]
+        i = df.loc[df.Day == Day]
         lectures.append(
             i.groupby(["Day", "Start", "End", "Unit"], as_index=False).agg(
                 {"Names": " ".join}
@@ -34,7 +34,7 @@ def group_names(df: pd.DataFrame):
     del df["Names"]
     df = df.drop_duplicates(["Day", "Start", "End", "Group", "Building"])
 
-    df["Day"] = pd.Categorical(df["Day"], categories=days, ordered=True)
+    df.Day = pd.Categorical(df.Day, categories=days, ordered=True)
     df = df.sort_values("Day").reset_index(drop=True)
 
     df = df.merge(lectures)
